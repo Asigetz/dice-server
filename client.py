3ac3ab95d097a593812c9cf5bba3de27937b2cfd
Asigetz <34882514+Asigetz@users.noreply.github.com>
2018-10-01 18:06:43 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,21 +1,10 @@
-# load additional Python modules
 import socket
 import json
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-local_hostname = socket.gethostname()
-local_fqdn = socket.getfqdn()
-ip_address = socket.gethostbyname(local_hostname)
-server_address = (ip_address, 8875)
-sock.connect(server_address)
-print ("connecting to %s (%s) with %s" % (local_hostname, local_fqdn, ip_address))
+data = {'app': 'asaf','throw': 1}
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect(('localhost', 8875))
+clientSocket.sendall(json.dumps(data))
+respond = clientSocket.recv(1024)
 
-data = {
-    'app': 'asaf',
-    'throw': 1
-}
-sock.sendall(json.dumps(data))
-newData = sock.recv(1024)
-sock.close()
-print 'Received', repr(newData)
-
+print('Received', repr(respond))
