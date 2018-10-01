da81a1839239d875ee29aefbb77b6a2b57e49036
Asigetz <34882514+Asigetz@users.noreply.github.com>
2018-10-01 19:59:30 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,4 @@
-import httplib, urllib
-
-params = urllib.urlencode({'app': 'asaf', 'throw': 1})
-headers = {"Content-type": "application/json", "Accept": "application/json"}
-conn = httplib.HTTPConnection('localhost', 8875)
-conn.request("POST", "/cgi-bin/query", params, headers)
-response = conn.getresponse()
-print response.status, response.reason
-
-doc = conn.getresponse().read()
-print doc
-data = response.read()
-conn.close()
+import requests
+payLoad = {'app': 'asaf', 'throw': 1}
+r = requests.get('http://localhost:8875', params = payLoad)
+print (r.content);
