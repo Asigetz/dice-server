71e9d2e9ea550a34600f67bff2a7d8f06817188d
Asig <asafgetzz@gmail.com>
2018-10-02 15:40:28 +0300

    extracted only numbers (throw results) from server response

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import requests
 import threading
+import json
 import Queue
 
 threads = []
@@ -9,8 +10,8 @@ results = []
 def get_request(throw_number):
     pay_load = {'app': 'asaf', 'throw': throw_number}
     r = requests.get('http://localhost:8875', params=pay_load)
-    results.append(r.content)
-
+    resp_dict = json.loads(r.content)
+    results.append(resp_dict['result'])
 
 for i in range(5):
     t = threading.Thread(target=get_request, args=(i,))
@@ -19,6 +20,5 @@ for i in range(5):
     t.join()
 
 print results
-
 
 
