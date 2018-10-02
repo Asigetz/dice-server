4364a527f3e1953e462a81d6a9fe23376c59c3fc
Asig <asafgetzz@gmail.com>
2018-10-02 14:08:04 +0300

    changed get_request to be multi-threaded.

--- client.py
+++ client.py
@@ -1,4 +1,22 @@
 import requests
-payLoad = {'app': 'asaf', 'throw': 1}
-r = requests.get('http://localhost:8875', params = payLoad)
-print (r.content);
+import threading
+
+results = []
+threads = []
+
+
+def get_request(throw_number):
+    pay_load = {'app': 'asaf', 'throw': throw_number}
+    r = requests.get('http://localhost:8875', params=pay_load)
+    results.append(r.content)
+
+
+for i in range(3):
+    t = threading.Thread(target=get_request, args=(i,))
+    threads.append(t)
+    t.start()
+
+
+print(results)
+
+
