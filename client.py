ceee5cd3e941a14ae28f81afd52b00699b8c1bed
Asig <asafgetzz@gmail.com>
2018-10-02 16:23:11 +0300

    added 50 requests run

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 import requests
 import threading
 import json
-import Queue
 
 threads = []
+results_round = []
 results = []
 
 
@@ -11,14 +11,21 @@ def get_request(throw_number):
     pay_load = {'app': 'asaf', 'throw': throw_number}
     r = requests.get('http://localhost:8875', params=pay_load)
     resp_dict = json.loads(r.content)
-    results.append(resp_dict['result'])
-
-for i in range(5):
-    t = threading.Thread(target=get_request, args=(i,))
-    threads.append(t)
-    t.start()
-    t.join()
-
-print results
+    results_round.append(resp_dict['result'])
 
 
+def throw_round(j):
+    for k in range(j, j + 5, 1):
+        t = threading.Thread(target=get_request, args=(k,))
+        threads.append(t)
+        t.start()
+        t.join()
+    return results_round;
+
+
+for i in range(0, 50, 5):
+    throw_round(i)
+    results.append(results_round)
+    results_round = []
+
+print(results)
